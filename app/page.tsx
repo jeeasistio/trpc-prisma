590bef5430fe6203dd5a2e2d94cae11bf1f94c48
Mahmoud <mhd-10@hotmail.com>
2022-11-07 12:07:36 +0300

    fixing issues with next-13

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,29 @@
-'use client'
-
-import { countries } from '@prisma/client'
-import Link from 'next/link'
-import { useState, ChangeEventHandler } from 'react'
-import { trpc } from '../helpers/trpc'
-import styles from '../styles/Home.module.css'
+import { countries } from '@prisma/client';
+import Link from 'next/link';
+import { useState, ChangeEventHandler } from 'react';
+import prismaClient from '../helpers/prisma';
+import { trpc } from '../helpers/trpc';
+import styles from '../styles/Home.module.css';
 
 // import styles from '../styles/Home.module.css'
 
-export default function Home() {
-    const [text, setText] = useState('')
-    // const { data, hasNextPage, fetchNextPage, isFetchingNextPage } = trpc.getAllCountries.useInfiniteQuery(
-    //     { name: text },
-    //     { getNextPageParam: (lastCursor) => lastCursor.nextCursor }
-    // )
+export default async function Home() {
+	const countries = await prismaClient.countries.findMany({ take: 100 });
+	return (
+		<div className={styles.container}>
+			{countries.map((country) => (
+				<div key={country.name}>
+					<Link href={`/${country.name}`}>
+						<p>{country.name}</p>
+					</Link>
+				</div>
+			))}
 
-    // const handleLoadMore = async () => {
-    //     await fetchNextPage()
-    // }
-
-    const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-        setText(e.target.value)
-    }
-
-    return (
-        <div className={styles.container}>
-            {/* {data?.pages
-                .reduce((acc, page) => {
-                    page.countries.forEach((country) => {
-                        acc.push(country)
-                    })
-
-                    return acc
-                }, [] as countries[])
-                .map((country) => (
-                    <div key={country.name}>
-                        <Link href={`/${country.name}`}>
-                            <p>{country.name}</p>
-                        </Link>
-                    </div>
-                ))}
-
-            <div>
-                <button disabled={!hasNextPage || isFetchingNextPage} onClick={handleLoadMore}>
-                    Load more
-                </button>
-            </div> */}
-        </div>
-    )
+			<div>
+				<button disabled={undefined} onClick={undefined}>
+					Load more
+				</button>
+			</div>
+		</div>
+	);
 }
