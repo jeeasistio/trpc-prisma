0ceb71787b248bdf41cfe8595b88e5b347b8111e
jeeasistio <jeeasistio08@gmail.com>
2022-11-10 18:48:53 +0800

    fixed csr[name]

--- app/csr/[name]/page.tsx
+++ app/csr/[name]/page.tsx
@@ -4,33 +4,6 @@ import { countries } from '@prisma/client'
 import { EditForm } from '../../../ui/EditForm'
 import { useEffect, useState } from 'react'
 
-// const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
-
-// const fetchFunc = async <T,>(name: string): Promise<T> => {
-//     const res = await fetch(`${baseUrl}/api/getCountry?name=${name}`)
-//     const data = await res.json()
-//     return data.country as T
-// }
-
-// interface Props {
-//     params: { name: string }
-// }
-// export default function Country({ params }: Props) {
-//     const { name, capital, continent, id, population, region } = use(fetchFunc<countries[][number]>(params.name))
-
-//     return (
-//         <div>
-//             <h1>{name}</h1>
-//             <p>Capital: {capital}</p>
-//             <p>Continent: {continent}</p>
-//             <p>Region: {region}</p>
-//             <p>Population: {population}</p>
-
-//             <EditForm country={{ name, capital, continent, population, region, id }} />
-//         </div>
-//     )
-// }
-
 interface Props {
     params: { name: string }
 }
@@ -45,8 +18,7 @@ export default function Country({ params }: Props) {
         }
 
         fetchFunc().then(() => setRerender(true))
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, [params.name])
 
     return (
         <div>
--- app/csr/page.tsx
+++ app/csr/page.tsx
@@ -4,33 +4,6 @@ import { countries } from '@prisma/client'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
-// const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
-
-// const fetchFunc = async <T,>(page?: number): Promise<T> => {
-//     const res = await fetch(`${baseUrl}/api/getCountries?page=${page}`)
-//     return await res.json()
-// }
-
-// export default function Home() {
-//     const [page, setPage] = useState(1)
-//     const countries = use(fetchFunc<countries[]>(page))
-
-//     const handleLoadMore = () => {
-//         setPage((prev) => prev + 1)
-//     }
-
-//     return (
-//         <div>
-//             {countries.map((country) => (
-//                 <Link key={country.name} href={`/csr/${country.name}`}>
-//                     <p>{country.name}</p>
-//                 </Link>
-//             ))}
-//             <button onClick={handleLoadMore}>Load More</button>
-//         </div>
-//     )
-// }
-
 export default function Home() {
     const [page, setPage] = useState(1)
     const [countries, setCountries] = useState<countries[]>([])
