4508a8ccb1dee129411f3961dc4637a04b22b6f3
jeeasistio <jeeasistio08@gmail.com>
2022-11-13 00:57:28 +0800

    fixed /csr/[name], used "use

--- app/csr/[name]/page.tsx
+++ app/csr/[name]/page.tsx
@@ -2,46 +2,43 @@
 
 import { countries } from '@prisma/client'
 import { EditForm } from '../../../ui/EditForm'
-import { useEffect, useState } from 'react'
+import { use } from 'react'
+
+interface Props {
+    params: { name: string }
+}
+
+const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
+
+const fetchFunc = async <T,>(name: string): Promise<T> => {
+    const res = await fetch(`${baseUrl}/api/getCountry?name=${name}`, { cache: 'no-store' })
+    return await res.json()
+}
+
+const cachedFetches = new Map<string, Promise<any>>()
+
+const queryClient = <QueryResult,>(name: string, query: () => Promise<QueryResult>): Promise<QueryResult> => {
+    if (!cachedFetches.has(name)) cachedFetches.set(name, query())
+    return cachedFetches.get(name)!
+}
 
 interface Props {
     params: { name: string }
 }
 export default function Country({ params }: Props) {
-    const [country, setCountry] = useState<countries>()
-    const [, setRerender] = useState(false)
-
-    useEffect(() => {
-        const fetchFunc = async () => {
-            const res = await fetch(`/api/getCountry?name=${params.name}`)
-            setCountry(await res.json())
-        }
-
-        fetchFunc().then(() => setRerender(true))
-    }, [params.name])
+    const { name, capital, continent, id, population, region } = use(
+        queryClient(`${params.name}`, () => fetchFunc<countries>(params.name))
+    )
 
     return (
         <div>
-            {country && (
-                <>
-                    <h1>{country.name}</h1>
-                    <p>Capital: {country.capital}</p>
-                    <p>Continent: {country.continent}</p>
-                    <p>Region: {country.region}</p>
-                    <p>Population: {country.population}</p>
+            <h1>{name}</h1>
+            <p>Capital: {capital}</p>
+            <p>Continent: {continent}</p>
+            <p>Region: {region}</p>
+            <p>Population: {population}</p>
 
-                    <EditForm
-                        country={{
-                            name: country.name,
-                            capital: country.capital,
-                            continent: country.continent,
-                            population: country.population,
-                            region: country.region,
-                            id: country.id,
-                        }}
-                    />
-                </>
-            )}
+            <EditForm country={{ name, capital, continent, population, region, id }} />
         </div>
     )
 }
--- ui/EditForm.tsx
+++ ui/EditForm.tsx
@@ -10,12 +10,13 @@ const fields = ['capital', 'continent', 'region', 'population'] as const
 interface FormBody extends Omit<countries, 'id' | 'name'> {}
 interface DataBody extends Omit<countries, 'name'> {}
 
-const fetchFunc = async (body: DataBody) => {
+const fetchFunc = async (body: DataBody, refresh: () => void) => {
     const res = await fetch('/api/updateCountry', {
         method: 'PUT',
         body: JSON.stringify(body),
     })
     const data = await res.json()
+    refresh()
     return data
 }
 
@@ -38,8 +39,7 @@ export const EditForm = ({ country }: { country: countries }) => {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        await fetchFunc({ ...data, id: country.id, population: Number(data.population) })
-        router.refresh()
+        await fetchFunc({ ...data, id: country.id, population: Number(data.population) }, router.refresh)
     }
 
     return (
